fee0ff777699b2973bdfd7a29691188ab6504aad
rfrenia <ryanfrenia@gmail.com>
2022-06-21 14:01:14 -0400

    create blank spaces

--- mystery_word.py
+++ mystery_word.py
@@ -1,5 +1,16 @@
 def play_game():
-    pass
+    open_file = open('test-word.txt')
+    #print(open_file.read())
+    string_word = str(open_file.read())
+    #print(string_word)
+    
+    def split(string_word):
+        return list(string_word)
+    split_list = split(string_word)
+    print(split_list)
+    new_list = [(item.replace(item, "_")) for item in split_list]
+    print(new_list)
+
 
 
 if __name__ == "__main__":
